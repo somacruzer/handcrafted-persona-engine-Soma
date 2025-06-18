5d44e05a3b66a884079f3fa6c166bcd857ab31de
Yunxing liu <somacruzer@foxmail.com>
2025-06-18 22:21:17 +0200

    feat: add THA4 Spout receiver

---
+++ src/PersonaEngine/PersonaEngine.Lib/Configuration/Tha4Options.cs
@@ -0,0 +1,10 @@
+namespace PersonaEngine.Lib.Configuration;
+
+public class Tha4Options
+{
+    public string SenderName { get; set; } = "THA4Avatar";
+
+    public int Width { get; set; } = 512;
+
+    public int Height { get; set; } = 512;
+}
